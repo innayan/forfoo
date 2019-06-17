847d3c8eeccce2f9ea0bc187efc22d5112bd95c3
innayan <innayan@TeamCity>
2019-06-17 12:25:57 +0300

    TeamCity change in 'TW-60797' project: runners of 'xxx' build template were updated

--- .teamcity/patches/templates/Xxx.kts
+++ .teamcity/patches/templates/Xxx.kts
@@ -2,6 +2,7 @@ package patches.templates
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.Template
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_2.ui.*
 
 /*
@@ -12,5 +13,13 @@ in the root project, and delete the patch script.
 create(DslContext.projectId, Template({
     id("Xxx")
     name = "xxx"
+
+    steps {
+        script {
+            name = "from template"
+            id = "RUNNER_11"
+            scriptContent = "echo template"
+        }
+    }
 }))
 
